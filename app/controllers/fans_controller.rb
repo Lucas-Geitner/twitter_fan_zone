45e421ba07ee62194564ffe3fbb36ac1eec2dbe7
Florian Cousin <floriancousin@hotmail.com>
2016-09-07 10:19:49 +0200

    change bigint to string, and add to_s on controller

--- app/controllers/fans_controller.rb
+++ app/controllers/fans_controller.rb
@@ -4,9 +4,9 @@ class FansController < ApplicationController
   @tweets = []
 
   @client.search("to:benoithamon", result_type: "recent").take(5).collect do |tweet|
-      if tweet.id != Post.where(tweet_id: tweet.id)
+      if tweet.id.to_s != Post.where(tweet_id: tweet.id.to_s)
       # Le tweet n'à pas encore été rajoutée.
-      @post = Post.create(tweet_id: tweet.id, fan_id: tweet.user.id)
+      @post = Post.create(tweet_id: tweet.id.to_s, fan_id: tweet.user.id.to_s)
       if tweet.user.id != Fan.where(name: tweet.user.name)
         # Premier tweet d'un fan
         @fan = Fan.create(name: tweet.user.name, category: "unknow", contact: "Pas encore contacté")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160906145448) do
+ActiveRecord::Schema.define(version: 20160907081313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,8 +24,8 @@ ActiveRecord::Schema.define(version: 20160906145448) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.bigint   "fan_id"
-    t.bigint   "tweet_id"
+    t.string   "fan_id"
+    t.string   "tweet_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
